2230632dd23bf4875b9291ecc20a6f4e9ea95db5
Ali Saif <145758021+AliSaif1@users.noreply.github.com>
2025-01-05 03:29:23 +0500

    Update firebase.js

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAallQC2v7H_4MQ5gBZo-OrxD_WZvc8FeU",
-  authDomain: "influencer-e6302.firebaseapp.com",
-  projectId: "influencer-e6302",
-  storageBucket: "influencer-e6302.appspot.com",
-  messagingSenderId: "177609698618",
-  appId: "1:177609698618:web:3750af679ab0677e093403",
-  measurementId: "G-P9R7X60576"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
